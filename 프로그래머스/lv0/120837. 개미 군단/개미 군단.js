04dc98776d3c86f0fd1aaad3dc04dff0f7001122
jongmi jung <116716381+jjo-mi@users.noreply.github.com>
2023-04-10 10:45:41 +0900

    [level 0] Title: 개미 군단, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,6 +1,8 @@
 function solution(hp) {
+    
   const 장군개미 = parseInt(hp / 5);
   const 병정개미 = parseInt((hp - 장군개미 * 5) / 3);
   const 일개미 = hp - 장군개미 * 5 - 병정개미 * 3;
   return 장군개미 + 병정개미 + 일개미;
+ 
 }
