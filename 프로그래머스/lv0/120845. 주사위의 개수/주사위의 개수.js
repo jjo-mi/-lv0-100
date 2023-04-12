da8735a12cd2865fba250d852286b6a15466118a
jongmi jung <116716381+jjo-mi@users.noreply.github.com>
2023-04-12 22:41:22 +0900

    [level 0] Title: 주사위의 개수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
@@ -0,0 +1,6 @@
+function solution(box, n) {
+    let answer = Math.floor(box[0] / n) *Math.floor(box[1] / n) *Math.floor(box[2] / n) 
+
+    
+    return box.reduce((acc, cur) => acc *= (Math.floor(cur / n)),1);
+}
