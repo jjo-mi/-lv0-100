6379d312cc1ec0ec0dd1931d861d599c393a71c2
jongmi jung <116716381+jjo-mi@users.noreply.github.com>
2023-04-12 23:28:00 +0900

    두번째 for문에 j<=i로 되어있어 자기자신으로 나누는값도 들어가서 수정함

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -1,8 +1,7 @@
 function solution(n) {
     var answer = new Set();
     for(let i = 4; i <= n; i++) {
-        for(let j = 2; j <= i; j++) {
-            if(i === j) continue;
+        for(let j = 2; j < i; j++) {
             if(i % j === 0){
                 answer.add(i);
             }
