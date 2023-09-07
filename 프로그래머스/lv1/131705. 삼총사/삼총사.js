32b80726295be2486fa08134a41f242d2139c309
jongmi jung <116716381+jjo-mi@users.noreply.github.com>
2023-09-07 23:00:31 +0900

    [level 1] Title: 삼총사, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -1,20 +1,15 @@
 function solution(number) {
-   const getCombinations = function (arr, selectNumber) {
-    const results = [];
-    if (selectNumber === 1) return arr.map((el) => [el]); 
-    arr.forEach((fixed, index, origin) => {
-      const rest = origin.slice(index + 1); 
-      const combinations = getCombinations(rest, selectNumber - 1); 
-      const attached = combinations.map((el) => [fixed, ...el]);
-      results.push(...attached); 
-    });                                    
-    return results; 
-  }
-
-   let cnt = 0;
-  const resultArr = getCombinations(number, 3);
-    resultArr.map(el =>{
-        if(0 === el[0]+ el[1]+ el[2]) cnt++
-    })
-    return cnt;
+    let count = 0;
+    // 모든 가능한 3개 조합 탐색
+    for(let i = 0; i < number.length - 2; i++) {
+        for(let j = i + 1; j < number.length - 1; j++) {
+            for(let k = j + 1; k < number.length; k++) {
+                if(number[i] + number[j] + number[k] === 0) {
+                    count++;
+                }
+            }
+        }
+    }
+    
+    return count;
 }
