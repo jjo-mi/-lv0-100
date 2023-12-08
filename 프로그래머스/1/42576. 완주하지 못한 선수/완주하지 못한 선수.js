8271bfd423a67c69950fdf07f7cfb4a958c11df6
jongmi jung <116716381+jjo-mi@users.noreply.github.com>
2023-12-09 01:05:33 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 75.11 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,12 +1,4 @@
 function solution(participant, completion) {
-    let map = new Map();
-    let arr = [...participant,...completion]
-    
-    for(let one of arr) {
-        map.has(one) ?  map.delete(one): map.set(one,1) ;
-    }
-
-    let answer = Array.from(map.keys()).join(',')
-    
+    var answer = '';
     return answer;
 }
